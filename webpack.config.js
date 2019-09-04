8b04538d589196e76249b6573e37b5a595a66637
Beth-zhao <zhaojiahui_22@163.com>
2019-09-04 15:38:25 +0800

    npm i -D extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -6,11 +7,18 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
     },
     module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader?minimize'] //loader的执行顺序是由后到前
-            }
-        ]
-    }
-}
+      rules: [
+        {
+            test: /\.css$/,
+            loaders:ExtractTextPlugin({
+                use: ['css-loader'] //loader的执行顺序是由后到前
+            })
+        }
+      ],
+      plugins: [
+        new ExtractTextPlugin({
+            filename: `[name]_[contenthash:8].css`,
+        })
+      ],
+    },
+};
